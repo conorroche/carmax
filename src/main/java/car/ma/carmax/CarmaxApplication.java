7b70449dee7e0580c752d303e3c2ed46fe712cc6
conorroche <conoroche@gmail.com>
2016-02-26 13:05:36 +0000

    docker integration

---
+++ src/main/java/car/ma/carmax/CarmaxApplication.java
@@ -0,0 +1,13 @@
+package car.ma.carmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmaxApplication.class, args);
+	}
+
+}
